6df13dd4af38ff66fcfbb0a50cfddb56d6c69799
AT0139 <takumi.study@outlook.jp>
2021-12-06 12:12:58 +0900

    zipline描画　完成

--- GO_SS_ZipLine.h
+++ GO_SS_ZipLine.h
@@ -8,6 +8,8 @@ struct VERTEX_ZIPLINE :public VERTEX_NOMAL
 	bool use;
 
 	D3DXVECTOR2 poleB_pos;
+	int poleBdistance;
+	float  angle;
 };
 
 class GO_SS_ZipLine :public GameObject
@@ -23,15 +25,16 @@ public:
 	void SetZipLine(D3DXVECTOR2 pos);
 	void AddX(FLOAT x);
 
-	void LinkZipLineB(D3DXVECTOR2 pos);
+	void LinkZipLineB(D3DXVECTOR2 pos, int poleBdistance);
 private:
 	const int GAME_SCENE = GAMESCENE_GAME_TEST;
 
 	int poleTex;
+	int wireTex;
 
 	static const int ZIPLINE_MAX = 5;		//ジップライン一対の最大数
 	const int POLE_MAX = ZIPLINE_MAX * 2;	//柱の最大数 **変更しない**
 	VERTEX_ZIPLINE ziplineInfo[ZIPLINE_MAX];
 
-	const int POLE_WALL_HEIGHT = 3;			//柱の縦マス数
+	const int POLE_WALL_HEIGHT = 2;			//柱の縦マス数
 };
